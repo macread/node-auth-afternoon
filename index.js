f9585a695f835aa239020caddfaa1e3f2245f033
Mac Read <james.macgregor.read@gmail.com>
2018-06-06 14:09:22 -0600

    Through step 3

--- index.js
+++ index.js
@@ -1,8 +1,17 @@
+require('dotenv').config();
+
 const express = require('express');
 const session = require('express-session');
 
 const app = express();
+const {
+    SERVER_PORT,
+    SESSION_SECRET,
+    DOMAIN,
+    CLIENT_ID,
+    CLIENT_SECRET,
+    CALLBACK_URL
+} = process.env;
 
 
-const port = 3000;
-app.listen( port, () => { console.log(`Server listening on port ${port}`); } );
+app.listen( SERVER_PORT, () => { console.log(`Server listening on port ${SERVER_PORT}`); } );
