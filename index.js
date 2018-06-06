8c06932f5da445ed58986a281a8edd296748252c
Mac Read <james.macgregor.read@gmail.com>
2018-06-06 14:24:10 -0600

    Through Step 4

--- index.js
+++ index.js
@@ -3,6 +3,10 @@ require('dotenv').config();
 const express = require('express');
 const session = require('express-session');
 
+const passpport = require('passport');
+const Auth0Strategy = require('passport-auth0');
+
+
 const app = express();
 const {
     SERVER_PORT,
@@ -12,6 +16,26 @@ const {
     CLIENT_SECRET,
     CALLBACK_URL
 } = process.env;
+
+app.use(session({
+    secret: SESSION_SECRET,
+    resave: false,
+    saveUninitialized: true
+}))
+
+app.use(passport.initialize());
+app.use(passport.session());
+
+passport.use(new Auth0Strategy({
+    domain: DOMAIN,
+    clientID: CLIENT_ID,
+    clientSecret: CLIENT_SECRET,
+    callbackURL: CALLBACK_URL,
+    scope: 'openid email profile'
+}, (accessToken, refreshToken, extraParams, profile, done) => {
+    done(null, profile);
+}))
+
 
 
 app.listen( SERVER_PORT, () => { console.log(`Server listening on port ${SERVER_PORT}`); } );
